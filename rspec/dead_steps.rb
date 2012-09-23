641c57f4830478023a506cce45dc04d61ff26088
Robert Cochran <cochrarj@muohio.edu>
2012-09-23 18:29:18 -0400

    Removed a dead file

--- rspec/dead_steps.rb
+++
@@ -1,8 +0,0 @@
-#../features/step_definitions/dead_step_sorter_steps/complex_calculator_steps.rb
-Then /^the result is "([^"]*)"$/ do |result|
-  result.to_i.should == @first_number + @second_number
-end
-#../features/step_definitions/dead_step_sorter_steps/nested_steps/nested_steps.rb
-Given /^I am a nested step$/ do
-  puts "i have no functionality"
-end
