875965c351c30b4eb67ac57a29266ffbb3557f8c
Christopher Vaughn <vaughncm@gmail.com>
2013-04-27 13:28:50 -0400

    Reorganized rules config list to be easier to navigate

--- lib/cuke_sniffer/rule_config.rb
+++ lib/cuke_sniffer/rule_config.rb
@@ -15,31 +15,169 @@ module CukeSniffer
     # Small improvements that can be made
     INFO = 1
 
-    # Master hash used for rule data
-    # * +:enabled+
-    # * +:phrase+
-    # * +:score+
-    # Optional:
-    # * +:words+
-    # * +:max+
-    # * +:min+
+    fatal_rules = {
+        :no_examples => {
+            :enabled => true,
+            :phrase => "Scenario Outline with no examples.",
+            :score => FATAL
+        },
+        :no_examples_table => {
+            :enabled => true,
+            :phrase => "Scenario Outline with no examples table.",
+            :score => FATAL
+        },
+        :recursive_nested_step => {
+            :enabled => true,
+            :phrase => "Recursive nested step call.",
+            :score => FATAL
+        },
+        :background_with_tag => {
+            :enabled => true,
+            :phrase => "There is a background with a tag. This feature file cannot run!",
+            :score => FATAL
+        },
+        :comment_after_tag => {
+            :enabled => true,
+            :phrase => "Comment comes between tag and properly executing line. This feature file cannot run!",
+            :score => FATAL
+        }
+    }
 
-    RULES = {
+    error_rules = {
+        :no_description => {
+            :enabled => true,
+            :phrase => "{class} has no description.",
+            :score => ERROR
+        },
+        :no_scenarios => {
+            :enabled => true,
+            :phrase => "Feature with no scenarios.",
+            :score => ERROR
+        },
+        :commented_step => {
+            :enabled => true,
+            :phrase => "Commented step.",
+            :score => ERROR
+        },
+        :commented_example => {
+            :enabled => true,
+            :phrase => "Commented example.",
+            :score => ERROR
+        },
+        :no_steps => {
+            :enabled => true,
+            :phrase => "No steps in Scenario.",
+            :score => ERROR
+        },
+        :one_word_step => {
+            :enabled => true,
+            :phrase => "Step that is only one word long.",
+            :score => ERROR
+        },
+        :no_code => {
+            :enabled => true,
+            :phrase => "No code in Step Definition.",
+            :score => ERROR
+        }
+    }
+
+    warning_rules = {
+        :numbers_in_description => {
+            :enabled => true,
+            :phrase => "{class} has numbers in the description.",
+            :score => WARNING
+        },
+        :empty_feature => {
+            :enabled => true,
+            :phrase => "Feature file has no content.",
+            :score => WARNING
+        },
+        :background_with_no_scenarios => {
+            :enabled => true,
+            :phrase => "Feature has a background with no scenarios.",
+            :score => WARNING
+        },
+        :background_with_one_scenario => {
+            :enabled => true,
+            :phrase => "Feature has a background with one scenario.",
+            :score => WARNING
+        },
+        :too_many_steps => {
+            :enabled => true,
+            :phrase => "{class} with too many steps.",
+            :score => WARNING,
+            :max => 7
+        },
+        :out_of_order_steps => {
+            :enabled => true,
+            :phrase => "Scenario steps out of Given/When/Then order.",
+            :score => WARNING
+        },
+        :invalid_first_step => {
+            :enabled => true,
+            :phrase => "Invalid first step. Began with And/But.",
+            :score => WARNING
+        },
+        :asterisk_step => {
+            :enabled => true,
+            :phrase => "Step includes a * instead of Given/When/Then/And/But.",
+            :score => WARNING
+        },
+        :one_example => {
+            :enabled => true,
+            :phrase => "Scenario Outline with only one example.",
+            :score => WARNING
+        },
+        :too_many_examples => {
+            :enabled => true,
+            :phrase => "Scenario Outline with too many examples.",
+            :score => WARNING,
+            :max => 10
+        },
+        :multiple_given_when_then => {
+            :enabled => true,
+            :phrase => "Given/When/Then used multiple times in the same {class}.",
+            :score => WARNING
+        },
+        :too_many_parameters => {
+            :enabled => true,
+            :phrase => "Too many parameters in Step Definition.",
+            :score => WARNING,
+            :max => 4
+        },
+        :lazy_debugging => {
+            :enabled => true,
+            :phrase => "Lazy Debugging through puts, p, or print",
+            :score => WARNING
+        },
+        :pending => {
+            :enabled => true,
+            :phrase => "Pending step definition. Implement or remove.",
+            :score => WARNING
+        },
+        :feature_same_tag => {
+            :enabled => true,
+            :phrase => "Same tag appears on Feature: ",
+            :score => WARNING
+        },
+        :scenario_same_tag => {
+            :enabled => true,
+            :phrase => "Tag appears on all scenarios: ",
+            :score => WARNING
+        },
+        :commas_in_description => {
+            :enabled => true,
+            :phrase => "There are commas in the description, creating possible multirunning scenarios or features.",
+            :score => WARNING
+        }
+    }
+
+    info_rules = {
         :too_many_tags => {
             :enabled => true,
             :phrase => "{class} has too many tags.",
             :score => INFO,
             :max => 8
-        },
-        :no_description => {
-            :enabled => true,
-            :phrase => "{class} has no description.",
-            :score => ERROR,
-        },
-        :numbers_in_description => {
-            :enabled => true,
-            :phrase => "{class} has numbers in the description.",
-            :score => WARNING,
         },
         :long_name => {
             :enabled => true,
@@ -53,139 +191,26 @@ module CukeSniffer
             :score => INFO,
             :words => ["page", "site", "url", "button", "drop down", "dropdown", "select list", "click", "text box", "radio button", "check box", "xml", "window", "pop up", "pop-up", "screen"]
         },
-        :empty_feature => {
-            :enabled => true,
-            :phrase => "Feature file has no content.",
-            :score => WARNING,
-        },
-        :background_with_no_scenarios => {
-            :enabled => true,
-            :phrase => "Feature has a background with no scenarios.",
-            :score => WARNING,
-        },
-        :background_with_one_scenario => {
-            :enabled => true,
-            :phrase => "Feature has a background with one scenario.",
-            :score => WARNING,
-        },
-        :no_scenarios => {
-            :enabled => true,
-            :phrase => "Feature with no scenarios.",
-            :score => ERROR,
-        },
         :too_many_scenarios => {
             :enabled => true,
             :phrase => "Feature with too many scenarios.",
             :score => INFO,
-            :max => 10,
-        },
-        :too_many_steps => {
-            :enabled => true,
-            :phrase => "{class} with too many steps.",
-            :score => WARNING,
-            :max => 7,
-        },
-        :out_of_order_steps => {
-            :enabled => true,
-            :phrase => "Scenario steps out of Given/When/Then order.",
-            :score => WARNING,
-        },
-        :invalid_first_step => {
-            :enabled => true,
-            :phrase => "Invalid first step. Began with And/But.",
-            :score => WARNING,
-        },
-        :asterisk_step => {
-            :enabled => true,
-            :phrase => "Step includes a * instead of Given/When/Then/And/But.",
-            :score => WARNING,
-        },
-        :commented_step => {
-            :enabled => true,
-            :phrase => "Commented step.",
-            :score => ERROR,
-        },
-        :commented_example => {
-            :enabled => true,
-            :phrase => "Commented example.",
-            :score => ERROR,
-        },
-        :no_examples => {
-            :enabled => true,
-            :phrase => "Scenario Outline with no examples.",
-            :score => FATAL,
-        },
-        :one_example => {
-            :enabled => true,
-            :phrase => "Scenario Outline with only one example.",
-            :score => WARNING,
-        },
-        :no_examples_table => {
-            :enabled => true,
-            :phrase => "Scenario Outline with no examples table.",
-            :score => FATAL,
-        },
-        :too_many_examples => {
-            :enabled => true,
-            :phrase => "Scenario Outline with too many examples.",
-            :score => WARNING,
             :max => 10
         },
         :date_used => {
             :enabled => true,
             :phrase => "Date used.",
-            :score => INFO,
-        },
-        :no_steps => {
-            :enabled => true,
-            :phrase => "No steps in Scenario.",
-            :score => ERROR,
-        },
-        :one_word_step => {
-            :enabled => true,
-            :phrase => "Step that is only one word long.",
-            :score => ERROR,
-        },
-        :multiple_given_when_then => {
-            :enabled => true,
-            :phrase => "Given/When/Then used multiple times in the same {class}.",
-            :score => WARNING,
-        },
-        :no_code => {
-            :enabled => true,
-            :phrase => "No code in Step Definition.",
-            :score => ERROR,
-        },
-        :too_many_parameters => {
-            :enabled => true,
-            :phrase => "Too many parameters in Step Definition.",
-            :score => WARNING,
-            :max => 4
+            :score => INFO
         },
         :nested_step => {
             :enabled => true,
             :phrase => "Nested step call.",
-            :score => INFO,
-        },
-        :recursive_nested_step => {
-            :enabled => true,
-            :phrase => "Recursive nested step call.",
-            :score => FATAL,
+            :score => INFO
         },
         :commented_code => {
             :enabled => true,
             :phrase => "Commented code in Step Definition.",
-            :score => INFO,
-        },
-        :lazy_debugging => {
-            :enabled => true,
-            :phrase => "Lazy Debugging through puts, p, or print",
-            :score => WARNING,
-        },
-        :pending => {
-            :enabled => true,
-            :phrase => "Pending step definition. Implement or remove.",
-            :score => WARNING,
+            :score => INFO
         },
         :small_sleep => {
             :enabled => true,
@@ -202,34 +227,22 @@ module CukeSniffer
         :todo => {
             :enabled => true,
             :phrase => "Todo found. Resolve it.",
-            :score => INFO,
-        },
-        :feature_same_tag => {
-            :enabled => true,
-            :phrase => "Same tag appears on Feature: ",
-            :score => WARNING,
-        },
-        :scenario_same_tag => {
-            :enabled => true,
-            :phrase => "Tag appears on all scenarios: ",
-            :score => WARNING,
-        },
-        :background_with_tag => {
-            :enabled => true,
-            :phrase => "There is a background with a tag. This feature file cannot run!",
-            :score => FATAL
-        },
-        :commas_in_description => {
-            :enabled => true,
-            :phrase => "There are commas in the description, creating possible multirunning scenarios or features.",
-            :score => WARNING
-        },
-        :comment_after_tag => {
-            :enabled => true,
-            :phrase => "Comment comes between tag and properly executing line. This feature file cannot run!",
-            :score => FATAL
+            :score => INFO
         }
     }
 
+    # Master hash used for rule data
+    # * +:enabled+
+    # * +:phrase+
+    # * +:score+
+    # Optional:
+    # * +:words+
+    # * +:max+
+    # * +:min+
+    RULES = {}
+      .merge fatal_rules
+      .merge error_rules
+      .merge warning_rules
+      .merge info_rules
   end
 end
