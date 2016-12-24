a418aa4e5f4421e235d1c8bf357290c7b1a771cd
Eric Kessler <morrow748@gmail.com>
2016-12-23 21:18:40 -0500

    Fixing a rule.
    Updated the 'out of order steps' rule so that it does not fire for a
    test that merely repeats keywords because this is already and more
    appropriately covered by a multiple keyword rule.

--- lib/cuke_sniffer/rule_config.rb
+++ lib/cuke_sniffer/rule_config.rb
@@ -199,6 +199,7 @@ module CukeSniffer
             :targets => ["Scenario"],
             :reason => lambda { |scenario, rule| step_order = scenario.get_step_order
                         ["But", "*", "And"].each { |type| step_order.delete(type) }
+                        step_order = step_order.chunk { |keyword| keyword }.map(&:first)
                         if(step_order != %w(Given When Then) and step_order != %w(When Then))
                           scenario.store_rule(rule)
                         end}
